b0ec5884ff98774b5f9e32abacdac7efcfd63df9
HyunMok-Chung <1021hm@naver.com>
2023-08-16 13:37:08 +0900

    refactor: pull request test2

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("merge commit test");
-        System.out.println("change main for Pull Request");
+        System.out.println("change main for Pull Request2");
     }
 }
